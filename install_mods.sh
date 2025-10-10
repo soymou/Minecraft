48e9da2528b48d055954bc126f6a0e8bfb8fe90c
soymou <emilio.junoy@gmail.com>
2025-10-09 23:18:06 -0600

    COGO

--- install_mods.sh
+++ install_mods.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 # Extract URLs from JSON and run packwiz modrinth install for each
-jq -r '.[].url' 1.20.1.json | while read -r url; do
+while read -r url; do
     echo "Installing: $url"
-    cd Server && packwiz modrinth install "$url"
+    cd Cogo && packwiz modrinth install "$url" </dev/tty
     cd ..
-done
+done < <(jq -r '.[].url' Cogo.json)
